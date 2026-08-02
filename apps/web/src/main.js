e51c8fbe8f40e740db8242beccc2af67f47d2cca
Justine Tablan <j.tablan@Justines-MacBook-Air.local>
2026-08-02 22:09:11 +0800

    Add role-aware dashboard with whole-request states

--- apps/web/src/main.js
+++ apps/web/src/main.js
@@ -7,5 +7,6 @@ import './assets/ui.css'
 import './assets/shell.css'
 import './assets/sidebar.css'
 import './assets/topbar.css'
+import './assets/dashboard.css'
 
 createApp(App).use(router).mount('#app')
